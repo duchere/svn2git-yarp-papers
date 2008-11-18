0cbbf09d329c7d45637154fd5029eba4e3cb647a
Claudio Castellini <claudio72@users.sourceforge.net>
2008-11-18 15:50:35 +0000

    reshaping for JNER submission

---
+++ 2008.BioCyb/figures/doit.bat
@@ -0,0 +1,35 @@
+latex figure_1
+dvips figure_1
+
+latex figure_2
+dvips figure_2
+
+latex figure_3
+dvips figure_3
+
+latex figure_4
+dvips figure_4
+
+latex figure_5
+dvips figure_5
+
+latex figure_6
+dvips figure_6
+
+latex figure_7
+dvips figure_7
+
+latex figure_8
+dvips figure_8
+
+latex figure_9
+dvips figure_9
+
+latex figure_10
+dvips figure_10
+
+latex figure_11
+dvips figure_11
+
+latex figure_12
+dvips figure_12
